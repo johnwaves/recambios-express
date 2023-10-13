3683f9a22c4cea4ddacec66bfae2fb0fe05ab9a8
Carlos Servillera <carlosservillera@gmail.com>
2023-10-13 18:32:02 +0200

    Solucionado Repartidor #13 closes Repartidor #13

---
+++ repartidor/repartidor.go
@@ -0,0 +1,26 @@
+package repartidor
+
+type Repartidor struct {
+    id      int
+    horario Horario
+}
+
+type Horario struct {
+    inicio string
+    fin    string
+}
+
+func NuevoRepartidor(id int, horario Horario) *Repartidor {
+    return &Repartidor{
+        id:      id,
+        horario: horario,
+    }
+}
+
+func (r *Repartidor) ID() int {
+    return r.id
+}
+
+func (r *Repartidor) Horario() Horario {
+    return r.horario
+}
