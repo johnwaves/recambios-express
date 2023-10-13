694bdb65e3471218a66c52028c941d250f0011e1
Carlos Servillera <carlosservillera@gmail.com>
2023-10-13 18:31:03 +0200

    Solucionado Pedidos #11 closes Pedidos #11

---
+++ pedido/pedido.go
@@ -0,0 +1,28 @@
+package pedido
+
+import ("recambios-express/pkg/repartidor")
+
+type Pedido struct {
+    id        int
+    clienteID int
+    repartidor Repartidor
+}
+
+func NuevoPedido(id, clienteID int) *Pedido {
+    return &Pedido{
+        id:        id,
+        clienteID: clienteID,
+    }
+}
+
+func (p *Pedido) ID() int {
+    return p.id
+}
+
+func (p *Pedido) ClienteID() int {
+    return p.clienteID
+}
+
+func (p *Pedido) AsignarRepartidor(r Repartidor) {
+    p.repartidor = r
+}
