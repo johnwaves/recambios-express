06af210ffca8b9c02658a264a3f0528b6be084c2
Carlos Servillera <carlosservillera@gmail.com>
2023-10-24 17:18:38 +0200

    Añadida variable nombre y su getter

--- src/pieza.go
+++ src/pieza.go
@@ -1,13 +1,18 @@
 package src
 
 type Pieza struct {
-	id int
+	id     int
+	nombre string
 }
 
 func NuevaPieza(id int) *Pieza {
 	return &Pieza{
 		id: id,
 	}
+}
+
+func (p *Pieza) NOMBRE() string {
+	return p.nombre
 }
 
 func (p *Pieza) ID() int {
