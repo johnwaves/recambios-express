441f94cb0ef32d12fd149a1cfbaea7e51a4a12d2
Carlos Servillera <carlosservillera@gmail.com>
2023-10-27 08:43:56 +0200

    Solucionado Arreglar nombre estructura JefeRecambios #29 Cambiado nombre de la estructura a Encargado

--- src/encargado.go
+++ src/encargado.go
@@ -1,11 +1,11 @@
 package src
 
-type JefeRecambios struct {
+type Encargado struct {
 	pedidosPendientes       []Pedido
 	repartidoresDisponibles []Repartidor
 	repartosActivos         map[*Pedido]map[*Repartidor]*HoraEntrega //Ojo con esto que son punteros para que sean comparables.
 }
 
-func NuevoJefeRecambios() *JefeRecambios {
-	return &JefeRecambios{}
+func NuevoEncargado() *Encargado {
+	return &Encargado{}
 }
