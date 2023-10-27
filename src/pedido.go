36730eacb83d50eef8da79e0fa9bd38f2c1a762c
Carlos Servillera <carlosservillera@gmail.com>
2023-10-27 10:20:49 +0200

    Solucionado Eliminar Objeto Pieza #30 No proporciona valor para el cliente por lo que hemos eliminado el objeto pieza.

--- src/pedido.go
+++ src/pedido.go
@@ -2,14 +2,12 @@ package tienda
 
 type Pedido struct {
 	id        int
-	piezas    map[Pieza]int
 	ubicacion UbicacionCliente
 }
 
-func NuevoPedido(id int, piezas map[Pieza]int, ubicacion UbicacionCliente) *Pedido {
+func NuevoPedido(id int, ubicacion UbicacionCliente) *Pedido {
 	return &Pedido{
 		id:        id,
-		piezas:    piezas,
 		ubicacion: ubicacion,
 	}
 }
