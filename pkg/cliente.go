d45784a5834c5854a2dd015ea9cf4ad28a3dd73a
Carlos Servillera <carlosservillera@gmail.com>
2023-10-30 15:40:09 +0100

    reopen #19 He incluido toda la información que necesitamos saber acerca del cliente para poder meter su pedido en ruta para entregarselo.

--- pkg/cliente.go
+++ pkg/cliente.go
@@ -1,21 +1,10 @@
 package pkg
 
 type Cliente struct {
-	nombreCliente         string
-	calle                 string
-	numero                int
-	poblacion             string
-	codigoPostal          int
-	tiempoEstimadoMinutos int
-}
-
-func NuevoCliente(nombreCliente string, calle string, numero int, poblacion string, codigo_postal int, tiempo_estimado_minutos int) *Cliente {
-	return &Cliente{
-		nombreCliente:         nombreCliente,
-		calle:                 calle,
-		numero:                numero,
-		poblacion:             poblacion,
-		codigoPostal:          codigo_postal,
-		tiempoEstimadoMinutos: tiempo_estimado_minutos,
-	}
+	nombre_cliente          string
+	calle                   string
+	numero                  int
+	poblacion               string
+	codigo_postal           int
+	tiempo_estimado_minutos int
 }
