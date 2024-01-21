1ac510e4d26f859ee2f075f960b657ddd69a6b9c
Mihnea Vidican <jvidican103@gmail.com>
2024-01-21 20:38:22 +0100

    Elimina campos innecesarios #49
    Los campos relacionados con la dirección del cliente se mueven a un
    nuevo tipo llamado Dirección. Por otra parte, en la estructura Cliente
    no es necesario almacenar tiempo_estimado_minutos. Esta variable resulta
    inútil tenerla aquí, pues es necesaria en una estructura donde se encuentre
    cada pedido almacenado junto a su tiempo estimado de entrega.

--- pkg/cliente.go
+++ pkg/cliente.go
@@ -1,10 +1,6 @@
 package pkg
 
 type Cliente struct {
-	nombre_cliente          string
-	calle                   string
-	numero                  int
-	poblacion               string
-	codigo_postal           int
-	tiempo_estimado_minutos int
+	nombre_cliente    string
+	direccion_entrega Direccion
 }
