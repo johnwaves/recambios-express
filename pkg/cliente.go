70f5e7d4664f930349522edcc2c7195eb5ca095d
Mihnea Vidican <jvidican103@gmail.com>
2024-01-22 23:18:42 +0100

    Añade método para crear un nuevo cliente #49

--- pkg/cliente.go
+++ pkg/cliente.go
@@ -4,3 +4,10 @@ type Cliente struct {
 	nombre_cliente    string
 	direccion_entrega Direccion
 }
+
+func NewCliente(nombre_cliente string, direccion_entrega Direccion) *Cliente {
+	return &Cliente{
+		nombre_cliente:    nombre_cliente,
+		direccion_entrega: direccion_entrega,
+	}
+}
