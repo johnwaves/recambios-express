bd72e5c9cc584d01433e685f4f5333bf378b759c
Mihnea Vidican <jvidican103@gmail.com>
2024-01-23 00:28:07 +0100

    Añade métodos para obtener dirección #50

--- pkg/cliente.go
+++ pkg/cliente.go
@@ -11,3 +11,11 @@ func NewCliente(nombre_cliente string, direccion_entrega Direccion) *Cliente {
 		direccion_entrega: direccion_entrega,
 	}
 }
+
+func (c *Cliente) Nombre() string {
+	return c.nombre_cliente
+}
+
+func (c *Cliente) FormatDireccionCliente() string {
+	return c.direccion_entrega.FormatDireccion()
+}
