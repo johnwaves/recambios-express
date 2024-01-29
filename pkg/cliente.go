e0ce4113f4cc8f1e1765067340faf06311f7a70f
Mihnea Vidican <jvidican103@gmail.com>
2024-01-29 23:12:33 +0100

    Añade verificaciones #51
    Se añaden varias verificaciones para notificar los errores que puedan
    existir al crear un cliente y un pedido antes de llamar al método
    AddPedido de Ruta.

--- pkg/cliente.go
+++ pkg/cliente.go
@@ -1,15 +1,39 @@
 package pkg
 
+import "errors"
+
 type Cliente struct {
 	nombre_cliente    string
 	direccion_entrega Direccion
 }
 
-func NewCliente(nombre_cliente string, direccion_entrega Direccion) *Cliente {
+func NewCliente(nombre_cliente string, direccion_entrega Direccion) (*Cliente, error) {
+
+	if nombre_cliente == "" {
+		return nil, errors.New("el nombre del cliente no puede estar vacío.")
+	}
+
+	errorMsg := ""
+	if direccion_entrega.calle_numero == "" {
+		errorMsg += "la calle del cliente no puede estar vacía. "
+	}
+
+	if direccion_entrega.poblacion == "" {
+		errorMsg += "la población del cliente no puede estar vacía. "
+	}
+
+	if direccion_entrega.codigo_postal == 0 || direccion_entrega.codigo_postal < 0 {
+		errorMsg += "el código postal del cliente no puede ser cero o negativo."
+	}
+
+	if errorMsg != "" {
+		return nil, errors.New("ERROR en la dirección del cliente: " + errorMsg)
+	}
+
 	return &Cliente{
 		nombre_cliente:    nombre_cliente,
 		direccion_entrega: direccion_entrega,
-	}
+	}, nil
 }
 
 func (c *Cliente) Nombre() string {
@@ -20,8 +44,13 @@ func (c *Cliente) FormatDireccionCliente() string {
 	return c.direccion_entrega.FormatDireccion()
 }
 
-func (c *Cliente) HacerPedido(items []ItemPedido) *Pedido {
-	pedido := NewPedido(items)
+func (c *Cliente) HacerPedido(items []ItemPedido) (*Pedido, error) {
+	pedido, err := NewPedido(items)
+	if err != nil {
+		return nil, err
+	}
+
 	pedido.asignarCliente(c)
-	return pedido
+
+	return pedido, nil
 }
--- pkg/pedido.go
+++ pkg/pedido.go
@@ -1,14 +1,39 @@
 package pkg
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type Pedido struct {
 	cliente *Cliente
 	items   []ItemPedido
 }
 
-func NewPedido(items []ItemPedido) *Pedido {
+func NewPedido(items []ItemPedido) (*Pedido, error) {
+	if len(items) == 0 {
+		return nil, errors.New("el pedido debe contener al menos un ítem")
+	}
+
+	var errorMessages []string
+
+	for i, item := range items {
+		if item.pieza == "" {
+			errorMessages = append(errorMessages, fmt.Sprintf("el nombre de la pieza en la posición %d no puede estar vacío.", i+1))
+		}
+		if item.cantidad <= 0 {
+			errorMessages = append(errorMessages, fmt.Sprintf("la cantidad de la pieza '%s' en la posición %d debe ser mayor que cero.", item.pieza, i+1))
+		}
+	}
+
+	if len(errorMessages) > 0 {
+		return nil, errors.New(strings.Join(errorMessages, "; "))
+	}
+
 	return &Pedido{
 		items: items,
-	}
+	}, nil
 }
 
 func (p *Pedido) asignarCliente(c *Cliente) {
